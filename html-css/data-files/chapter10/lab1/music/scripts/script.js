7897fb0095fa52afa055d899cee651f8a1ef8a3f
Thomas Schulte <ts2378202@gmail.com>
2025-09-28 09:28:17 -0500

    Commit CH10 Lab 1

---
+++ html-css/data-files/chapter10/lab1/music/scripts/script.js
@@ -0,0 +1,16 @@
+/*
+Thomas Schulte
+script.js
+9/28/2025
+*/
+
+// Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
