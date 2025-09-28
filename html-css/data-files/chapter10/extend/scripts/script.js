45f46b1322e5f228decc585d4fbeded25570fe41
Thomas Schulte <ts2378202@gmail.com>
2025-09-28 07:38:28 -0500

    Commit CH10 Extend

--- html-css/data-files/chapter10/extend/scripts/script.js
+++ html-css/data-files/chapter10/extend/scripts/script.js
@@ -1,5 +1,17 @@
 /*
-    Student Name:
+    Student Name: Thomas Schulte
     File Name: script.js
-    Date: 
+    Date: 9/28/2025
 */
+
+$(document).ready(function() {
+    // Hide paragraphs when "Hide" button is clicked
+    $("button:contains('Hide')").click(function() {
+        $("#main p").hide();
+    });
+
+    // Show paragraphs when "Show" button is clicked
+    $("button:contains('Show')").click(function() {
+        $("#main p").show();
+    });
+});
