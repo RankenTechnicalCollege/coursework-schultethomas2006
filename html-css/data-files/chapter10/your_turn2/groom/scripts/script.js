a4e76a6928738fc75f4cecfa45fc45e6ae324ff1
Thomas Schulte <ts2378202@gmail.com>
2025-09-28 23:20:49 -0500

    Commit CH10 Groom

---
+++ html-css/data-files/chapter10/your_turn2/groom/scripts/script.js
@@ -0,0 +1,15 @@
+//Hamburger Menu function
+function hamburger() {
+    var navlinks = document.getElementById("nav-links");
+    var menuicon = document.getElementById("icon");
+
+    if (navlinks.style.display === "block") {
+        navlinks.style.display = "none";
+        menuicon.style.color = "#111111";
+        menuicon.classList.remove("active"); //remove transform
+    } else {
+        navlinks.style.display = "block";
+        menuicon.style.color = "#ff6600";
+        menuicon.classList.add("active"); //apply transform
+    }
+}
