62f4955799c99c8e83383ae4a7e0111047faa179
Thomas Schulte <ts2378202@gmail.com>
2025-10-26 13:42:06 -0500

    Commit JavaScript CH1

--- javascript/data-files/Chapter01/js01/project02/project01-02_txt.js
+++ javascript/data-files/Chapter01/js01/project02/project01-02_txt.js
@@ -2,10 +2,13 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Thomas Schulte
+    Date: 10/26/25
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+var service1Name="Basic", service2Name="Express", service3Name="Extreme", service4Name="Ultimate";
+
+var service1Speed="0 Mbps", service2Speed="100 Mbps", service3Speed="500 Mbps", service4Speed="1 Gig";
--- javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,12 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Thomas Schulte
+    Date:   10/26/25
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
 
+var homeStreet="1 Main St. ", homeCity="Sicilia", homeState="MA ", homeCode="02103 ", workStreet="15 Oak Ln.", workCity="Central City", workState="MA ", workCode="02104";
